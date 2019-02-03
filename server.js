c103936249e1516e98f4db436508ee12d08a063b
Kimberly Santiago <kdsanman@gmail.com>
2019-02-03 10:04:56 -0500

    last push

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const app = express();
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
 
-  twiml.message('Thank you for signing up! You will now start receiving daily notifications on how to be sustainable.');
+  twiml.message('Thank you for signing up! You will now start receiving daily notifications on how to be sustainable. Are you at DEVFEST2019? Start being more sustainable by reusing your coffee cup!  --Hungry Hippos');
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
+
 });
 
 http.createServer(app).listen(1337, () => {
