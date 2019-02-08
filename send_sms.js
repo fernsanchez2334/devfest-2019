5f75bf8358516b63cedec75f93dc57b8438f3ac2
Kim <kdsanman@gmail.com>
2019-02-08 18:48:58 -0500

    Update send_sms.js

--- send_sms.js
+++ send_sms.js
@@ -1,7 +1,7 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
-const accountSid = 'ACbc81b70cab7b0c42a0c3bb3b0624bae6';
-const authToken = '5e5c5250251b3d2bbbb98ad103ae21a5';
+const accountSid = '=';
+const authToken = '';
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
